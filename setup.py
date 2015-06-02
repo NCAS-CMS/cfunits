b829c6a539f398709077c8b617c75bf8044514e7
david <d.c.hassell@reading.ac.uk>
2015-06-02 09:54:03 +0100

    README

--- setup.py
+++ setup.py
@@ -30,8 +30,25 @@ etc_files    = [f for f in find_package_data_files('cfunits/etc')]
 
 package_data = etc_files
 
-with open('README.md') as ldfile:
-    long_description = ldfile.read()
+#with open('README.md') as ldfile:
+#    long_description = ldfile.read()
+
+long_description = '''A python interface to UNIDATA's Udunits-2 package with CF
+extensions
+
+Store, combine and compare physical units and convert numeric values
+to different units.
+
+Units are as defined in UNIDATA's Udunits-2 package , except for
+reference time units (such as 'days since 2000-12-1' in the
+'proleptic_gregorian' calendar), which are handled by the netCDF4
+python package.
+
+In addition, some units are either new to, modified from, or removed
+from the standard Udunits-2 database in order to be more consistent
+with the CF conventions.'''
+
+# classifiers list at: https://pypi.python.org/pypi?%3Aaction=list_classifiers
 
 setup(name = "cfunits",
       long_description = long_description,
@@ -42,17 +59,20 @@ setup(name = "cfunits",
       url          = "https://bitbucket.org/cfpython/cfunits-python",
       download_url = "https://bitbucket.org/cfpython/cfunits-python/downloads",
       platforms    = ["Linux", "MacOS"],
-      license      = ["OSI Approved"],
-      keywords     = ['cf', 'udunits', 'numpy','netcdf','data','science','network','oceanography','meteorology','climate'],
-      classifiers  = ["Development Status :: 3 - Alpha",
+      license      = ["MIT"],
+      keywords     = ['cf', 'udunits','netcdf','data','science', 'oceanography', 'meteorology', 'climate'],
+      classifiers  = ["Development Status :: 5 - Production/Stable",
                       "Intended Audience :: Science/Research", 
-                      "License :: OSI Approved", 
+                      "License :: OSI Approved :: MIT License", 
                       "Topic :: Software Development :: Libraries :: Python Modules",
                       "Topic :: System :: Archiving :: Compression",
-                      "Operating System :: OS Independent"],
+                      "Operating System :: MacOS",
+                      "Operating System :: POSIX :: Linux",
+                      "Programming Language :: Python :: 2.6",
+                      "Programming Language :: Python :: 2.7",],
       packages     = ['cfunits'],
       package_data = {'cfunits': package_data},
-      requires     = ['netCDF4 (>=0.9.7)',
-                      'numpy (>=1.7)',                      
-                      ],
+      requires = ['netCDF4 (>=0.9.7)',
+                  'numpy (>=1.7)',                      
+              ],
   )
