eb22beb60c8aa30d96fc912418f5fd6a2daf4c82
Sadie Louise Bartholomew <sadie.bartholomew@ncas.ac.uk>
2020-11-02 22:08:32 +0000

    Remove duplicate author attribute on module

--- cfunits/__init__.py
+++ cfunits/__init__.py
@@ -16,7 +16,6 @@ with the CF conventions.
 
 __Conventions__ = 'CF-1.8'
 __author__ = 'David Hassell'
-__author__ = 'David Hassell'
 __date__ = '2020-10-09'
 __version__ = '3.3.0'
 __cf_version__ = '1.8'
