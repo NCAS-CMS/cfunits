740dd16854fba68726594bbe806435c712ba3d1e
Sadie Louise Bartholomew <sadie.bartholomew@ncas.ac.uk>
2021-01-07 18:03:36 +0000

    Apply black to cfunits project setup.py

--- setup.py
+++ setup.py
@@ -6,38 +6,40 @@ import sys
 import imp
 import re
 
+
 def find_package_data_files(directory):
     for root, dirs, files in os.walk(directory):
         for basename in files:
-            if fnmatch.fnmatch(basename, '*'):
+            if fnmatch.fnmatch(basename, "*"):
                 filename = os.path.join(root, basename)
-                yield filename.replace('cfunits/', '', 1)
+                yield filename.replace("cfunits/", "", 1)
+
 
 def _read(fname):
-    """Returns content of a file.
-
-    """
+    """Returns content of a file."""
     fpath = os.path.dirname(__file__)
     fpath = os.path.join(fpath, fname)
-    with open(fpath, 'r') as file_:
+    with open(fpath, "r") as file_:
         return file_.read()
 
-def _get_version():
-    """Returns library version by inspecting __init__.py file.
 
-    """
-    return re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-                     _read("cfunits/__init__.py"),
-                     re.MULTILINE).group(1)
-      
-version      = _get_version()
-packages     = ['cfunits']
-etc_files    = [f for f in find_package_data_files('cfunits/etc')]
-test_files    = [f for f in find_package_data_files('cfunits/test')]
+def _get_version():
+    """Returns library version by inspecting __init__.py file."""
+    return re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+        _read("cfunits/__init__.py"),
+        re.MULTILINE,
+    ).group(1)
+
+
+version = _get_version()
+packages = ["cfunits"]
+etc_files = [f for f in find_package_data_files("cfunits/etc")]
+test_files = [f for f in find_package_data_files("cfunits/test")]
 
 package_data = etc_files + test_files
 
-#with open('README.md') as ldfile:
+# with open('README.md') as ldfile:
 #    long_description = ldfile.read()
 
 long_description = """*A python interface to UNIDATA's UDUNITS-2 library with CF
@@ -73,32 +75,44 @@ https://ncas-cms.github.io/cfunits/installation.html
 # classifiers list at: https://pypi.python.org/pypi?%3Aaction=list_classifiers
 
 # Get dependencies
-requirements = open('requirements.txt', 'r')
-install_requires = requirements.read().splitlines() 
+requirements = open("requirements.txt", "r")
+install_requires = requirements.read().splitlines()
 
-setup(name = "cfunits",
-      long_description = long_description,
-      version      = version,
-      description  = "A python interface to UNIDATA's UDUNITS-2 package with CF extensions ",
-      maintainer   = "David Hassell",
-      author       = "David Hassell",
-      maintainer_email = "david.hassell@ncas.ac.uk",
-      author_email = "david.hassell@ncas.ac.uk",
-      url          = "https://bitbucket.org/cfpython/cfunits-python",
-      download_url = "https://bitbucket.org/cfpython/cfunits-python/downloads",
-      platforms    = ["Linux", "MacOS"],
-      license      = ["MIT"],
-      keywords     = ['cf', 'udunits', 'UNIDATA', 'netcdf','data',
-                      'science', 'oceanography', 'meteorology', 'climate'],
-      classifiers  = ["Development Status :: 5 - Production/Stable",
-                      "Intended Audience :: Science/Research", 
-                      "License :: OSI Approved :: MIT License", 
-                      "Topic :: Scientific/Engineering",
-                      "Operating System :: MacOS",
-                      "Operating System :: POSIX :: Linux",
-                      "Programming Language :: Python :: 3",],
-      packages     = ['cfunits'],
-      package_data = {'cfunits': package_data},
-      python_requires='>=3.6',
-      install_requires = install_requires,
-  )
+setup(
+    name="cfunits",
+    long_description=long_description,
+    version=version,
+    description="A python interface to UNIDATA's UDUNITS-2 package with CF extensions ",
+    maintainer="David Hassell",
+    author="David Hassell",
+    maintainer_email="david.hassell@ncas.ac.uk",
+    author_email="david.hassell@ncas.ac.uk",
+    url="https://bitbucket.org/cfpython/cfunits-python",
+    download_url="https://bitbucket.org/cfpython/cfunits-python/downloads",
+    platforms=["Linux", "MacOS"],
+    license=["MIT"],
+    keywords=[
+        "cf",
+        "udunits",
+        "UNIDATA",
+        "netcdf",
+        "data",
+        "science",
+        "oceanography",
+        "meteorology",
+        "climate",
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["cfunits"],
+    package_data={"cfunits": package_data},
+    python_requires=">=3.6",
+    install_requires=install_requires,
+)
