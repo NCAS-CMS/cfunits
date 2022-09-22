3ae6fbc52c1ad0ef733995f16c301ed7f4811759
David Hassell <david.hassell@ncas.ac.uk>
2022-09-22 16:23:50 +0100

    version/date

--- cfunits/__init__.py
+++ cfunits/__init__.py
@@ -15,8 +15,8 @@ with the CF conventions.
 """
 
 __Conventions__ = "CF-1.10"
-__date__ = "2021-08-09"
-__version__ = "3.3.4"
+__date__ = "2022-09-22"
+__version__ = "3.3.5"
 __cf_version__ = "1.10"
 
 import platform
